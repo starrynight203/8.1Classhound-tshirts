595a6100c635279b922793d0dc8bb536566659ce
Hannah Smith <starrynight203@msn.com>
2016-03-22 14:06:23 -0400

    trying to add and push

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -15,7 +15,26 @@ $('#add1').click(function(e){
   e.preventDefault();
   var name = $('.name').html()
   var quantity = $('.quantity').val()
-  var size = $('.size').html()
+  var size = $('.size').val()
+  console.log(name);
+  console.log(quantity);
+  console.log(size);
+});
+$('#add2').click(function(e){
+  e.preventDefault();
+  var name = $('.name2').html()
+  var quantity = $('.quantity2').val()
+  var size = $('.size2').val()
+  console.log(name);
+  console.log(quantity);
+  console.log(size);
+});
+
+$('#add3').click(function(e){
+  e.preventDefault();
+  var name = $('.name3').html()
+  var quantity = $('.quantity3').val()
+  var size = $('.size3').val()
   console.log(name);
   console.log(quantity);
   console.log(size);
